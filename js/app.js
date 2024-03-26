b7a01fdf4fe09aedfa2a29ca78b68d51db349c78
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-26 22:24:33 +0600

    categories btn section implemented

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('js linked');
+const loadBtnData = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+  const data = await res.json()
+  displayBtnData(data.data);
+}
+
+const displayBtnData = (data) => {
+  const btnContainer = document.getElementById('btn-container');
+  data.forEach(singleData => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <button onclick="loadCategoryData('${singleData.category}')" class="bg-secondary text-tertiary px-4 py-1 rounded-md visited:bg-primary">
+    ${singleData.category}
+    </button>
+    `
+    btnContainer.appendChild(div);
+
+  })
+}
+
+const loadCategoryData = (data) => {
+  console.log(data);
+}
+
+loadBtnData();
