273ada656b649fdf4ac25c777b5c6fcec75f7dd0
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-27 16:47:50 +0600

    dynamically card implemented

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const displayBtnData = (data) => {
   data.forEach(singleData => {
     const div = document.createElement('div');
     div.innerHTML = `
-    <button onclick="loadCategoryData('${singleData.category}')" class="bg-secondary text-tertiary px-4 py-1 rounded-md visited:bg-primary">
+    <button onclick="loadCategoryData('${singleData.category_id}')" class="bg-secondary text-tertiary px-4 py-1 rounded-md visited:bg-primary">
     ${singleData.category}
     </button>
     `
@@ -18,8 +18,56 @@ const displayBtnData = (data) => {
   })
 }
 
-const loadCategoryData = (data) => {
-  console.log(data);
+const loadCategoryData = async (id) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+  const data = await res.json()
+  displayCategoryData(data.data);
+}
+
+const displayCategoryData = (data) => {
+  const container = document.getElementById('card-container');
+  container.textContent = '';
+  data.forEach(singleCard => {
+    const { category_id, thumbnail, title } = singleCard;
+    const { posted_date, views } = singleCard?.others;
+    const { profile_name, profile_picture, verified } = singleCard?.authors[0];
+
+    const hours = Math.floor(posted_date / 3600);
+    const minutes = Math.floor((posted_date % 3600) / 60);
+
+    const div = document.createElement('div');
+    div.classList = `max-w-sm bg-white border border-gray-200 rounded-lg shadow`;
+    div.innerHTML = `
+    <a href="#" class='relative'>
+    <img
+    class = 'h-[200px] w-full'
+      src="${thumbnail}"
+      alt="image"
+    />
+    <div>
+      ${hours > 0 && minutes > 0 ? (`<p class='absolute bottom-3 rounded-md right-3 bg-black p-1 text-white'>${hours} hours ${minutes} minutes</p>`) : ('')}
+    </div>
+    </a>
+    <div class="p-5">
+    <div class="flex items-center gap-3">
+      <img class="rounded-full w-10 h-10" src="${profile_picture}" alt="logo">
+      <h5
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+      >
+        ${title}
+      </h5>
+    </div>
+    <p class="mb-3 font-normal text-gray-700">
+      ${profile_name} 
+      <span>${verified ? "<i class='bx bxs-check-circle text-blue-500'></i>" : ""}</span>
+    </p>
+    <p class="mb-3 font-normal text-gray-700">
+      ${views} views
+    </p>
+    </div>
+    `
+    container.appendChild(div);
+  })
 }
 
 loadBtnData();
